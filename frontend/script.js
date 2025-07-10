a4299d243f22dd077825d96929d366772083cad2
danielc149 <danielc149@gmail.com>
2025-07-09 20:51:33 -0400

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,21 @@
 
 const characters = [
-    { name: "Pasta Pope", rarity: "Legendario" },
-    { name: "Mamma Mafia", rarity: "Épico" },
-    { name: "Vespa Cat", rarity: "Raro" },
-    { name: "Chianti Vampire", rarity: "Raro" },
+    { name: "Pasta Pope", rarity: "Raro" },
+    { name: "Mamma Mafia", rarity: "Raro" },
+    { name: "Vespa Cat", rarity: "Común" },
+    { name: "Chianti Vampire", rarity: "Común" },
     { name: "Opera Ghost", rarity: "Común" },
     { name: "Pizza Gladiator", rarity: "Común" }
+    { name: "Tralalero Tralala", rarity: "Legendario" }
+    { name: "Tung Tung Tung Sahur", rarity: "Legendario" }
+    { name: "Bombardiro Crocodilo", rarity: "Legendario" }
+    { name: "Frigo Cammello Buffo Fardello", rarity: "Legendario" }
+    { name: "Bailarina Cappuccina", rarity: "Legendario" }
+    { name: "Lirilì Larilà, rarity: "Épico" }
+    { name: "Chimpanzini Bananini", rarity: "Épico" }
+    { name: "Shpioniro Golubiro", rarity: "Épico" }
+    { name: "Tripi Tripo", rarity: "Épico" }
+    { name: "Bombombini Gusini", rarity: "Épico" }
 ];
 
 function pullGacha() {
